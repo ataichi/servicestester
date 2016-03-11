0c51b59ddf4ef44e714daffab26005471462dd56
unknown <patricia_nieva@dlsu.edu.ph>
2016-03-12 00:42:56 +0800

    parser from danica g

--- src/main/java/Servlet/LanguageTranslationServlet.java
+++ src/main/java/Servlet/LanguageTranslationServlet.java
@@ -12,7 +12,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import org.json.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.JSONParser;
 
 @WebServlet(name = "LanguageTranslationServlet", urlPatterns = {"/LanguageTranslationServlet"})
   
@@ -40,7 +43,7 @@ public class LanguageTranslationServlet extends HttpServlet {
 			
 			request.setAttribute("outputText",translatedText);
 			
-			
+			/*
 			JSONObject obj = new JSONObject(translatedText);
 			final JSONArray trans = obj.getJSONArray("translations");
 			final int n = trans.length();
@@ -48,7 +51,23 @@ public class LanguageTranslationServlet extends HttpServlet {
 				final JSONObject words = trans.getJSONObject(i);
 				db.insertInto(input, words.getString("translation"));
 				
-			}
+			}*/
+			
+			String langtrans = translatedText;
+            
+            JSONParser parser = new JSONParser();
+            Object obj = parser.parse(langtrans);
+            
+            JSONObject jsonObject = (JSONObject) obj;
+            String character_count = jsonObject.get("character_count").toString();
+      
+            JSONArray jsonArray = (JSONArray) jsonObject.get("translations");
+            JSONObject jsonObject1 = (JSONObject) jsonArray.get(0);
+            String translation = jsonObject1.get("translation").toString();
+         
+            
+            String word_count = jsonObject.get("word_count").toString();
+           
 			
 			response.setContentType("text/html");
 			response.setStatus(200);
